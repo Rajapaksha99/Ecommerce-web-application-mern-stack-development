047c9b947195c0debc7c313187c5ffcfc9cb829a
Kavindu Ranganth <91959867+Rajapaksha99@users.noreply.github.com>
2024-12-23 10:54:01 +0530

    make admin panel successfully

--- admins/src/Components/ListProduct/ListProduct.jsx
+++ admins/src/Components/ListProduct/ListProduct.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './ListProduct.css'
-import cross_icon from '../../assets/'
+import cross_icon from '../../assets/cross_icon.png'
 
 const ListProduct = () => {
 
@@ -16,8 +16,30 @@ const ListProduct = () => {
     fetchInfo();
   },[])
 
+  const remove_product = async (id) => {
+    try {
+        const response = await fetch('http://localhost:4000/removeproduct', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ id }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        await fetchInfo(); // Refresh the product list
+    } catch (error) {
+        console.error("Failed to remove product:", error);
+    }
+};
+
+
   return (
-    <div className='list-product'>
+    <div className='listproduct'>
       <h1>All Product List</h1>
       <div className="listproduct-format-main">
         <p>Products</p>
@@ -30,14 +52,17 @@ const ListProduct = () => {
       <div className="listproduct-allproduct">
         <hr />
         {allproducts.map((product,index)=>{
-          return <div key={index} className='listproduct-format-main listproduct-format'>
+          return <> 
+            <div key={index} className='listproduct-format-main listproduct-format'>
               <img className='listproduct-product-icon' src={product.image} alt="" />
               <p>{product.name}</p>
               <p>${product.old_price}</p>
               <p>${product.new_price}</p>
               <p>{product.category}</p>
-              <img className='listproduct-remove-icon' src="" alt="" />
+              <img onClick={()=>{remove_product(product.id)}} className='listproduct-remove-icon' src={cross_icon} alt="" />
             </div>
+            <hr />
+          </>
         })}
       </div>
     </div>
